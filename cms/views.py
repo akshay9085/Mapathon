ab3b6907e174f98f05e6602f57824135be700edb
Sashi20 <gayatrirekha204@gmail.com>
2020-11-26 16:07:43 +0530

    First Commit

---
+++ cms/views.py
@@ -0,0 +1,31 @@
+# Create your views here.
+from django.forms import model_to_dict
+from django.http import Http404
+from django.shortcuts import render
+
+from cms.models import Page, Nav, SubNav, Footer, Banner
+
+
+def home(request, permalink=''):
+    if permalink == '':
+        permalink = 'home'
+    page = Page.objects.filter(permalink=permalink, active=True)
+    footer = Footer.objects.filter(active=True)
+    banner = Banner.objects.filter(active=True)
+    if page.exists():
+        page = page.first()
+    else:
+        raise Http404("The requested page does not exists")
+    nav_objs = Nav.objects.filter(active=True).order_by('position')
+
+    navs = []
+
+    for nav in nav_objs:
+        nav_obj = model_to_dict(nav)
+        nav_obj['subnavs'] = nav.subnav_set.filter(
+            active=True).order_by('position')
+        navs.append(nav_obj)
+
+    return render(
+        request, 'cms_base.html', {'page': page, 'navs': navs, 'footer': footer.first(), 'banner' : banner.first()}
+    )
---
+++ mapathon/config.py
@@ -0,0 +1,40 @@
+DB_NAME_DEFAULT = 'dbname'
+DB_USER_DEFAULT = 'root'
+DB_PASS_DEFAULT = 'root'
+DB_HOST_DEFAULT = 'localhost'
+DB_PORT_DEFAULT = ''
+SECRET_KEY_VAL = 'secret-key'
+########################################
+GOOGLE_KEY = 'GOOGLE_KEY'
+GOOGLE_SECRET = 'GOOGLE_SECRET'
+FACEBOOK_KEY = 'FACEBOOK_KEY'
+FACEBOOK_SECRET = 'FACEBOOK_SECRET'
+GITHUB_KEY = 'GITHUB_KEY'
+GITHUB_SECRET = 'GITHUB_SECRET'
+RECAPTCHA_PUBLIC = 'RECAPTCHA_PUBLIC'
+RECAPTCHA_PRIVATE = 'RECAPTCHA_PRIVATE'
+SYSADMINS = [('SYSADMINS')]
+SYS_SERVER_EMAIL = 'SYS_SERVER_EMAIL'
+ADMIN_EMAIL_ID = 'admin email'
+########################################
+TO_EMAIL = 'email@email.com'
+CC_EMAIL = ['email@email.com', 'idf@hgmz.com']
+BCC_EMAIL = 'BCC_EMAIL'
+SENDER_EMAIL = 'email@email.com'
+########################################
+# Host for sending e-mail.
+EMAIL_HOST_SERVER = 'localhost'
+# Port for sending e-mail.
+EMAIL_PORT_SERVER = 25
+#########################################
+# Optional SMTP authentication information for EMAIL_HOST.
+EMAIL_HOST_USER_SERVER = ''
+EMAIL_HOST_PASSWORD_SERVER = ''
+EMAIL_USE_TLS_SERVER = False
+#########################################
+ALLOWED_HOSTS_VAL = ['127.0.0.1','localhost']
+DEBUG_VAL = True
+SITE = 'SITE'
+PRODUCTION_URL_NAME = 'http://127.0.0.1:8000'
+ROOT_URL_VAL = 'http://127.0.0.1:8000' #http://127.0.0.1:8000
+#########################################
