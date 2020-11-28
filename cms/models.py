98d0b48507cf63f5a13f4a274c2fc11dd9b51fe5
Sashi20 <gayatrirekha204@gmail.com>
2020-11-28 10:35:13 +0530

    Update links to css and js in html

--- cms/models.py
+++ cms/models.py
@@ -4,6 +4,7 @@ from django.core.exceptions import ValidationError
 from django.core.files.storage import FileSystemStorage
 from django.db import models
 from django.conf import settings
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -89,3 +90,35 @@ class StaticFile(models.Model):
 
     def __str__(self):
         return self.filename
+
+
+@receiver(models.signals.post_delete, sender=StaticFile)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `StaticFile` object is deleted.
+    """
+    if instance.file:
+        if os.path.isfile(instance.file.path):
+            os.remove(instance.file.path)
+
+@receiver(models.signals.pre_save, sender=StaticFile)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    """
+    Deletes old file from filesystem
+    when corresponding `StaticFile` object is updated
+    with new file.
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = StaticFile.objects.get(pk=instance.pk).file
+    except StaticFile.DoesNotExist:
+        return False
+
+    new_file = instance.file
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
+
